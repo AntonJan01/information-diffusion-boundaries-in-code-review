959608cd03f6e37bea30391365b3e55159351f69
AntonJan01 <103118358+AntonJan01@users.noreply.github.com>
2023-05-17 11:43:32 +0200

    added unit test example

--- test/test_model.py
+++ test/test_model.py
@@ -15,6 +15,14 @@ class ModelTest(unittest.TestCase):
         self.assertEqual(len(ModelTest.cn.hyperedges()), 3)
         self.assertEqual(ModelTest.cn.hyperedges('v1'), {'h1'})
 
+class ModelTest2(unittest.TestCase):
+    cn = CommunicationNetwork({'h1': ['v1', 'v1'], 'h2': ['v2', 'v3','v4','v5','v6'], 'h3': ['v1', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+
+    def test_vertices(self):
+        self.assertEqual(len(ModelTest2.cn.vertices()), 6)
+        self.assertEqual(ModelTest2.cn.vertices('h1'),{'v1','v1'})
+        self.assertEqual(ModelTest2.cn.vertices('h1'),{'v1'})
+        self.assertEqual(ModelTest2.cn.vertices('h2'),{'v2', 'v3','v4','v5','v6'})
 
 class ModelDataTest(unittest.TestCase):
     def test_model_with_data(self):
