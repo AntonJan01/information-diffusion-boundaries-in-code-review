96694926b5ee7cf986e2b63e702c4253f3f5c90d
Donald <donaldelezi@icloud.com>
2023-05-27 15:21:45 +0200

    Update test_minimal_paths.py

--- test/test_minimal_paths.py
+++ test/test_minimal_paths.py
@@ -24,3 +24,67 @@ class MinimalPath(unittest.TestCase):
         result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
         result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
         self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+import unittest
+
+from simulation.model import CommunicationNetwork
+from simulation.minimal_paths import single_source_dijkstra_vertices, single_source_dijkstra_hyperedges, DistanceType
+
+
+class MinimalPath(unittest.TestCase):
+    cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+
+    def test_1(self):
+        self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0), {'v2': 1, 'v3': 2, 'v4': 3})
+
+    def test_2(self):
+        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
+        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
+        print('\n')
+        print(result_1)
+        print(result_2)
+        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+
+    def test_3(self):
+        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
+        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
+        print('\n')
+        print(result_1)
+        print(result_2)
+        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+
+    def test_4(self):
+        result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
+        result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
+        print('\n')
+        print(result_1)
+        print(result_2)
+        self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
+
+
+
+class MinimalPathExceptionHandeling(unittest.TestCase):
+
+    def test_minimal_path_unknown_vertice(self):
+        cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+
+        with self.assertRaises(Exception):
+            single_source_dijkstra_vertices(cn, 'v5', DistanceType.SHORTEST, min_timing=0)
+
+        with self.assertRaises(Exception):
+            single_source_dijkstra_hyperedges(cn, 'v5', DistanceType.SHORTEST, min_timing=0)
+
+
+    def test_minimal_path_unknown_distanceType(self):
+        cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+
+        with self.assertRaises(UnboundLocalError):
+            single_source_dijkstra_vertices(cn, 'v1', DistanceType, min_timing=0)
+
+        with self.assertRaises(UnboundLocalError):
+            single_source_dijkstra_hyperedges(cn, 'v1', DistanceType, min_timing=0)
+
+    def test_minimal_path_negative_min_timing(self):
+        cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+
+        with self.assertRaises(Exception):
+            single_source_dijkstra_vertices(cn, 'v1', DistanceType.FASTEST, min_timing=-1)
